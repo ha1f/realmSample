67fba3d77fa0b1b542b522588afdc0fa1c0c431b
ha1f <half.ldk.hiz@gmail.com>
2016-09-07 23:32:54 +0900

    sample動作確認

---
+++ realmSample/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  realmSample
+//
+//  Created by はるふ on 2016/09/07.
+//  Copyright © 2016年 はるふ. All rights reserved.
+//
+
+import RealmSwift
+
+class User: Object {
+    dynamic var id = 1
+    dynamic var name = ""
+}
--- realmSample/ViewController.swift
+++ realmSample/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let me = User()
+        me.name = "はるふ"
+        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(me)
+        }
+        
+        print(realm.objects(User))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
